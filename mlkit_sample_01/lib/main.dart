210f4252dee09ee633128b7bbf2f6773d1d87c2d
hmiyashita1 <hiroshi.miyashita@uniadex.co.jp>
2022-03-17 16:32:06 +0900

    顔識別用のコードを追加

--- mlkit_sample_01/lib/main.dart
+++ mlkit_sample_01/lib/main.dart
@@ -1,115 +1,160 @@
-import 'package:flutter/material.dart';
+import 'dart:io' as io;
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:image/image.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Face Finder",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.blueGrey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const FaceFinder(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class FaceFinder extends StatefulWidget {
+  const FaceFinder({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => _FaceFinderState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _FaceFinderState extends State<FaceFinder> {
+  io.File? _imageFile;
+  Size? _imageSize;
+  List<Face>? _faceResults;
+  final ImagePicker _picker = ImagePicker();
+  final FaceDetector _faceDetector = GoogleMlKit.vision.faceDetector();
 
-  void _incrementCounter() {
+  Future<void> _setFaceDetectResult(ImageSource imageSource) async {
+    final XFile? imgXFile = await _picker.pickImage(source: imageSource);
+    if (imgXFile == null) return;
+    final io.File imgFile = io.File(imgXFile.path);
+
+    final image = decodeImage(io.File(imgFile.path).readAsBytesSync());
+    if (image == null) return;
+    final imgSz = Size(image.width.toDouble(), image.height.toDouble());
+
+    final InputImage inpImg = InputImage.fromFile(imgFile);
+    final List<Face> faces = await _faceDetector.processImage(inpImg);
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _imageFile = imgFile;
+      _imageSize = imgSz;
+      _faceResults = faces;
     });
+  }
+
+  Widget _buildFaceDetectResult() {
+    final imgFile = _imageFile;
+    final imgSz = _imageSize;
+    final faces = _faceResults;
+    final boxDec = imgFile != null
+        ? BoxDecoration(
+            image: DecorationImage(
+            image: FileImage(imgFile),
+            fit: BoxFit.contain,
+          ))
+        : const BoxDecoration();
+    final result = imgFile != null && faces != null && imgSz != null
+        ? CustomPaint(
+            painter: FaceBorderDrawer(imgSz, faces),
+          )
+        : const Center(
+            child: Text(
+              "Detecting...",
+              style: TextStyle(color: Colors.blueGrey, fontSize: 32.0),
+            ),
+          );
+
+    return Container(
+      constraints: const BoxConstraints.expand(),
+      decoration: boxDec,
+      child: result,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text("Face Finder Sample"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: _imageFile == null
+          ? const Center(child: Text("No image selected."))
+          : _buildFaceDetectResult(),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => _setFaceDetectResult(ImageSource.gallery),
+            tooltip: "Select Image.",
+            child: const Icon(Icons.add_photo_alternate),
+          ),
+          FloatingActionButton(
+            onPressed: () => _setFaceDetectResult(ImageSource.camera),
+            tooltip: "Take a Photo.",
+            child: const Icon(Icons.add_a_photo),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class FaceBorderDrawer extends CustomPainter {
+  final Size orgImageSize;
+  final List<Face> faces;
+
+  FaceBorderDrawer(this.orgImageSize, this.faces);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final hasVSpace =
+        size.height >= (size.width / orgImageSize.width * orgImageSize.height);
+    final scale = hasVSpace
+        ? size.width / orgImageSize.width
+        : size.height / orgImageSize.height;
+    final space = hasVSpace
+        ? (size.height - orgImageSize.height * scale) / 2.0
+        : (size.width - orgImageSize.width * scale) / 2.0;
+    final Paint paint = Paint()
+      ..strokeWidth = 3
+      ..style = PaintingStyle.stroke
+      ..color = Colors.red;
+
+    for (Face face in faces) {
+      final bbox = face.boundingBox;
+      canvas.drawRect(
+        Rect.fromLTRB(
+          bbox.left * scale + (hasVSpace ? 0 : space),
+          bbox.top * scale + (hasVSpace ? space : 0),
+          bbox.right * scale + (hasVSpace ? 0 : space),
+          bbox.bottom * scale + (hasVSpace ? space : 0),
+        ),
+        paint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
